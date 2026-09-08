b535de49b5b3748c0ddc1c26ff2ae0eb801f3f3b
guromain <romain.guibert44@ynov.com>
2026-09-08 16:59:08 +0200

    [MODIF] la salle gère les scénarion en variable de class

--- app/Salle.py
+++ app/Salle.py
@@ -1,11 +1,12 @@
 class Salle:
 
-    def __init__(self,id,nom,etat,scenario,indice,codeSortie):
+    def __init__(self,id,nom,etat,scenario,indice,listeObj,codeSortie):
         self.id = id
         self.nom = nom
         self.etat = etat
         self.scenario = scenario
         self.indice = indice
+        self.listeObj = listeObj
         self.codeSortie = codeSortie
 
     def tenterEchapper(self,codeP):
