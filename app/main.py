1c00150070b11c9ea0e23f476240ff5d3c182b60
guromain <romain.guibert44@ynov.com>
2026-09-08 11:20:14 +0200

    First Commit Initialisation API basique

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="EscapeEngine API Test")
+
+@app.get("/")
+def read_root():
+    return {"status": "ok", "message": "Environnement Conda prêt pour l'Escape Game !"}
