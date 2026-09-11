2ffaa27a4aa948631f029c6eb3cdf4c545a3a6e3
guromain <romain.guibert44@ynov.com>
2026-09-11 14:43:41 +0200

    [MODIF] main.py premier get

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI(title="EscapeEngine API Test")
 @app.get("/")
 def read_root():
     return {"status": "ok", "message": "Environnement Conda prêt pour l'Escape Game !"}
+
+
+@app.get("/get-data-room/{room_id}")
+def get_data_room(room_id:int):
+    pass
