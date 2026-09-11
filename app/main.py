2ae55ade8c592291b39ffd27f49effbcdcc8c37c
guromain <romain.guibert44@ynov.com>
2026-09-11 17:26:06 +0200

    [MODIF] déplacement des route

--- app/main.py
+++ app/main.py
@@ -19,3 +19,16 @@ def start():
 @app.get("/get-data-room")
 def get_data_room():
     return game.get_data()
+
+
+@app.patch("/inventory/addItem/{itemId}")
+def addItem(itemId: int):
+    return {"Ajout": 'Item {itemId} ajouté à l\'inventaire'}
+
+@app.patch("/inventory/removeItem/{itemId}")
+def removeItem(itemId: int):
+    return {"Retrait": 'Item {itemId} retiré de l\'inventaire'}
+
+@app.get("/inventory/showInventory")
+def showInventory():
+    return {"Affichage": 'Affichage des items de l\'inventaire.'}
--- main.py
+++ main.py
@@ -2,14 +2,3 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Api de l'inventaire")
 
-@app.patch("/inventory/addItem/{itemId}")
-def addItem(itemId: int):
-    return {"Ajout": 'Item {itemId} ajouté à l\'inventaire'}
-
-@app.patch("/inventory/removeItem/{itemId}")
-def removeItem(itemId: int):
-    return {"Retrait": 'Item {itemId} retiré de l\'inventaire'}
-
-@app.get("/inventory/showInventory")
-def showInventory():
-    return {"Affichage": 'Affichage des items de l\'inventaire.'}
