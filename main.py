7335fccbdac34c1d77f012886e7797b46893cf66
guromain <romain.guibert44@ynov.com>
2026-09-24 10:01:33 +0200

    [DELETE] doublon de requêtes du aux merges

--- main.py
+++ main.py
@@ -44,15 +44,3 @@ def getInd():
 @app.get('/tryescape/{code}')
 def tryEscape(code:int):
     return game.levelChange(code)
-
-@app.patch("/inventory/addItem/{itemId}")
-def addItem(itemId: int):
-    return {"Ajout": 'Item {itemId} ajouté à l\'inventaire'}
-
-@app.patch("/inventory/removeItem/{itemId}")
-def removeItem(itemId: int):
-    return {"Retrait": 'Item {itemId} retiré de l\'inventaire'}
-
-@app.get("/inventory/showInventory")
-def showInventory():
-    return {"Affichage": 'Affichage des items de l\'inventaire.'}
